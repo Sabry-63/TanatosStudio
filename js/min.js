4300335507759e8989bb58ba3b70c9776130e158
Sabry-63 <sabrry0@gmail.com>
2020-06-04 20:29:47 +0200

    first commit

---
+++ js/min.js
@@ -0,0 +1,185 @@
+$(function() {
+    "use strict";
+
+    // Set Hieght For Header
+    $(".header").css("height", $(window).innerHeight());
+
+    // Start Function Fixed Mune ==============================
+    $(window).scroll(function() {
+        // Set Var
+        const scroll = $(this).scrollTop();
+        // Check Scroll Window
+        if (scroll > $(this).innerHeight()) {
+            // Set Navlist Top Page & Set Background To nav
+            $(".nav-bar").addClass("fixed-nav");
+            $(".header .nav-bar .list-nav").css("background", "transparent");
+
+            // Show Btn Top Page
+            $(".fa-angle-up").fadeIn();
+
+            // Check Widow Width & Set Background To Full Screen
+            if ($(window).innerWidth() > 992) {
+                $(".header .nav-bar .list-nav").css("background", "#f7600e");
+            }
+        } else {
+            // Remove Fixed Top Navlist
+            $(".nav-bar").removeClass("fixed-nav");
+
+            // Show Btn Top Page
+            $(".fa-angle-up").fadeOut();
+
+            // Check Window Width & Set Background To Navlist
+            if ($(window).innerWidth() > 992) {
+                $(".header .nav-bar .list-nav").css("background", "#f7600e");
+            } else {
+                $(".header .nav-bar .list-nav").css("background", "#000");
+            }
+        }
+    });
+    // End Function Fixed Mune ============================
+
+    //
+    $(".link-list").click(function(e) {
+        e.preventDefault();
+        $(this)
+            .parent()
+            .siblings()
+            .children()
+            .removeClass("active");
+        $(this).addClass("active");
+
+        //aintame to el
+        $("html, body").animate(
+            {
+                scrollTop: $("#" + $(this).data("scroll")).offset().top + 1
+            },
+            1000
+        );
+    });
+
+    //
+
+    // Start Toggele Navlist Function ==============================
+    // Create Var
+    const widthNavList = $(".header .list-nav").innerWidth();
+
+    // Hide Listnav In Full Screen
+    if ($(window).innerWidth() > 992) {
+        $(".header .list-nav").css("marginRight", -widthNavList);
+    } else {
+        // Show Navlist In Mobail Screen
+        $(".header .list-nav").css("marginRight", 0);
+    }
+
+    // Click To bars
+    $(".menu").click(function() {
+        $(".header .list-nav").toggleClass("show");
+        // Check Window Width
+        if ($(window).innerWidth() > 992) {
+            // Navlist Show Right For Animate
+            if ($(".header .list-nav").hasClass("show")) {
+                // Set Margin To Body
+                $("body").animate(
+                    {
+                        marginRight: widthNavList
+                    },
+                    500
+                );
+                // Show Listnav
+                $(".header .list-nav").animate(
+                    {
+                        marginRight: 0,
+                        opacity: 1
+                    },
+                    500
+                );
+            } else {
+                // Reomve Margin To Body
+                $("body").animate(
+                    {
+                        marginRight: 0
+                    },
+                    500
+                );
+                // Hide Listnav
+                $(".header .list-nav").animate(
+                    {
+                        marginRight: -widthNavList,
+                        opacity: 0
+                    },
+                    500
+                );
+            }
+        } else {
+            // Show & Hide Navlist To Mobail Screen
+            $(".header .list-nav").slideToggle();
+        }
+    });
+    // End Toggele Navlist Function =====================================
+
+    // Start Function Goto Section Features Shtmose ==============================
+    $(".header .fa-angle-down").click(function() {
+        $("html, body").animate(
+            {
+                scrollTop: $(".features").offset().top + 1
+            },
+            1000 // Timeing Function
+        );
+    });
+    // End Toggele Function Goto Section Features Shtmose ============================
+
+    // Start Function Testmonial ==============================
+
+    // Set Vars Arrow Click
+    const rightArrwo = $(".testmonial .right");
+    const leftArrwo = $(".testmonial .left");
+
+    // Function Check Arrow Show
+    function user() {
+        $(".usesr:first").hasClass("uesr-show")
+            ? leftArrwo.hide()
+            : leftArrwo.fadeIn();
+        $(".usesr:last").hasClass("uesr-show")
+            ? rightArrwo.hide()
+            : rightArrwo.fadeIn();
+    }
+
+    user(); // Run Function
+
+    // Click To Arrow RIght & Left
+    $(".testmonial i").click(function() {
+        // Click Arrow Right
+        $(this).hasClass("right")
+            ? // Hide The User And Show Next User
+              $(".usesr.uesr-show").fadeOut(300, function() {
+                  $(this)
+                      .removeClass("uesr-show")
+                      .next(".usesr")
+                      .addClass("uesr-show")
+                      .fadeIn(1000);
+                  user();
+              })
+            : // Click Arrow Left
+              // Hide The User And Show Next User
+              $(".testmonial .uesr-show").fadeOut(300, function() {
+                  $(this)
+                      .removeClass("uesr-show")
+                      .prev(".usesr")
+                      .addClass("uesr-show")
+                      .fadeIn(1000);
+                  user();
+              });
+    });
+    // End Function Testmonial  ============================
+
+    //
+    $(".fa-angle-up").click(function() {
+        $("html, body").animate(
+            {
+                scrollTop: 0
+            },
+            2000
+        );
+    });
+    //
+});
